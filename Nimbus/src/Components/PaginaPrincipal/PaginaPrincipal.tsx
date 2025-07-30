45b1bf602f03b37de5439aef437354e60e0c833d
Nicolas Duarte <nickboytube0418@gmail.com>
2025-07-29 23:24:59 -0300

    Fazendo o input personalizado

--- Nimbus/src/Components/PaginaPrincipal/PaginaPrincipal.tsx
+++ Nimbus/src/Components/PaginaPrincipal/PaginaPrincipal.tsx
@@ -2,13 +2,16 @@ import styles from "./PaginaPrincipal.module.css";
 import { useState } from "react";
 import axios from "axios";
 
+import { Search } from "lucide-react";
+
 export function PaginaPrincipal() {
     return (
         <div className={styles.container}>
             <div className={styles.leftCard}>
-                <h1 className={styles.titulo}>Bem-vindo ao Clima App</h1>
-                <p className={styles.descricao}>Aqui você pode consultar as condições climáticas atuais de qualquer cidade do mundo.</p>
-                <p className={styles.instrucoes}>Digite o nome da cidade no campo de busca para obter as informações climáticas.</p>
+                <div className={styles.searchContainer}>
+                    <Search className={styles.searchIcon} />
+                    <input className={styles.input} type="text" placeholder="Pesquise uma cidade . . ."/>
+                </div>
             </div>
             <div className={styles.rightCard}>
                 <h1 className={styles.titulo}>Bem-vindo ao Clima App</h1>
